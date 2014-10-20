618c1f1c8bcf79abde491f211e8e6e281af6d083
noaheli <noah.eli@gmail.com>
2014-10-20 15:43:24 -0500

    100 points
    FINALLY

--- lab11-100pt.py
+++ lab11-100pt.py
@@ -23,7 +23,7 @@ def animate():
     # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(circle)
     if x2 > drawpad.winfo_width(): 
-        direction = - 1
+        drawpad.move(circle, -800, 0)
     elif x1 < 0:
         direction = 1
     #Move our oval object by the value of direction
